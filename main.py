e0752529ff375dc5f0ff29aaaab738fab42e5386
Maxime Moreillon <m.moreillon@gmail.com>
2025-11-23 18:54:57 +0900

    minor

--- main.py
+++ main.py
@@ -2,41 +2,72 @@ from db import driver
 from os import path
 from markdownify import markdownify as md
 
-output_folder = "./output"
-limit = 3
+#
+# output_folder = "./output"
+output_folder = "/home/moreillon/dev/hugo/articles/content/articles"
 
 
-def save_html(article):
-    file_name = f"{article["_id"]}.html"
-    file_path = path.join(output_folder, "html", file_name)
-    file_content = article["content"]
+def process_content(content):
+    # Remove h1 (striped by md)
+    content = "\n".join(content.splitlines()[2:])
 
-    with open(file_path, "w") as file_object:
-        file_object.write(file_content)
-
-
-def save_md(article):
-
-    # file_name = f"{article["title"]}.md"
-    file_name = f"{article["_id"]}.md"
-    file_path = path.join(output_folder, "md", file_name)
-    # file_content = md(article["content"], strip=["h1"])
-    file_content = md(article["content"])
-
-    with open(file_path, "w") as file_object:
-        file_object.write("+++\n")
-        file_object.write(f"date = '2025-11-20T10:44:51+09:00'\n")
-        file_object.write(f"draft = true\n")
-        file_object.write(f"title = '{article["title"]}'\n")
-        file_object.write("+++\n")
-        file_object.write("\n")
-        file_object.write(file_content)
+    # Fix images query
+    content = content.replace(
+        "https://img.maximemoreillon.com/image?id=",
+        "https://img.maximemoreillon.com/images/",
+    )
+    return content
 
 
 with driver.session() as session:
-    cypher = f"MATCH (a:Article) RETURN PROPERTIES(a) as article LIMIT {limit}"
-    result = session.run(cypher)
-    for record in result:
-        article = record["article"]
-        save_html(article)
-        save_md(article)
+    cypher = """
+        MATCH (article:Article {published: true})
+        WITH article
+        OPTIONAL MATCH (tag:Tag)-[:APPLIED_TO]->(article)
+        WITH article, COLLECT(properties(tag)) AS tags
+        MATCH (article)-[authorship:WRITTEN_BY]->(author:User)
+
+        WITH COUNT(DISTINCT(article)) AS article_count,
+          COLLECT({
+            article: properties(article),
+            author: properties(author),
+            authorship: properties(authorship),
+            tags: tags
+          }) AS articles
+
+        RETURN articles
+        """
+    records = session.run(cypher)
+
+    for record in records:
+        items = record["articles"]
+        for item in items:
+
+            article = item["article"]
+
+            file_name = f"{article["_id"]}.md"
+            file_path = path.join(output_folder, file_name)
+
+            content = md(article["content"], strip=["h1"])
+
+            content = process_content(content)
+
+            tags = []
+            for tag in item["tags"]:
+                tags.append(tag["name"])
+
+            with open(file_path, "w") as file_object:
+                file_object.write("+++\n")
+                file_object.write(f'title = "{article["title"]}"\n')
+                file_object.write(f"date = '{item["authorship"]["creation_date"]}'\n")
+                file_object.write(f"lastmod = '{item["authorship"]["edition_date"]}'\n")
+                file_object.write(f"tags = {tags}\n")
+                # if "summary" in article:
+                #     file_object.write(f'summary = "{article["summary"]}"\n')
+                # file_object.write(f"draft = true\n")
+                file_object.write("+++\n")
+                file_object.write("\n")
+
+                file_object.write(content)
+
+            print(article["title"])
