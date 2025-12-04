7e911f3ed5b308fb9eda8bf3e3a16e0203ffd6f8
Maxime Moreillon <m.moreillon@gmail.com>
2025-12-04 09:39:33 +0900

    using with

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from db import driver
-from os import path
+from dotenv import load_dotenv
+from os import path, getenv
 from markdownify import markdownify as md
+from neo4j import GraphDatabase
+import re
+
+load_dotenv()
+
+NEO4J_URI = getenv("NEO4J_URI")
+NEO4J_USERNAME = getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = getenv("NEO4J_PASSWORD")
+
 
 #
 # output_folder = "./output"
 output_folder = "/home/moreillon/dev/hugo/articles/content/articles"
+
+
+# def find_image_manager_images(content):
+#     url_pattern = r"https?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+|(?:www\.)(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+#     urls = re.findall(url_pattern, content)
+#     for url in urls:
+#         if url.startswith("https://img.maximemoreillon.com"):
+#             print(url)
 
 
 def process_content(content):
@@ -16,58 +33,69 @@ def process_content(content):
         "https://img.maximemoreillon.com/image?id=",
         "https://img.maximemoreillon.com/images/",
     )
+
+    # find_image_manager_images(content)
+
     return content
 
 
-with driver.session() as session:
-    cypher = """
-        MATCH (article:Article {published: true})
-        WITH article
-        OPTIONAL MATCH (tag:Tag)-[:APPLIED_TO]->(article)
-        WITH article, COLLECT(properties(tag)) AS tags
-        MATCH (article)-[authorship:WRITTEN_BY]->(author:User)
+with GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USERNAME, NEO4J_PASSWORD)) as driver:
+    with driver.session() as session:
 
-        WITH COUNT(DISTINCT(article)) AS article_count,
-          COLLECT({
-            article: properties(article),
-            author: properties(author),
-            authorship: properties(authorship),
-            tags: tags
-          }) AS articles
+        cypher = """
+            MATCH (article:Article {published: true})
+            WITH article
+            OPTIONAL MATCH (tag:Tag)-[:APPLIED_TO]->(article)
+            WITH article, COLLECT(properties(tag)) AS tags
+            MATCH (article)-[authorship:WRITTEN_BY]->(author:User)
 
-        RETURN articles
-        """
-    records = session.run(cypher)
+            WITH COUNT(DISTINCT(article)) AS article_count,
+            COLLECT({
+                article: properties(article),
+                author: properties(author),
+                authorship: properties(authorship),
+                tags: tags
+            }) AS articles
 
-    for record in records:
-        items = record["articles"]
-        for item in items:
+            RETURN articles
+            """
 
-            article = item["article"]
+        records = session.run(cypher)
 
-            file_name = f"{article["_id"]}.md"
-            file_path = path.join(output_folder, file_name)
+        # NOTE: there is only one record for this query
+        for record in records:
+            items = record["articles"]
+            for item in items:
 
-            content = md(article["content"], strip=["h1"])
+                article = item["article"]
 
-            content = process_content(content)
+                file_name = f"{article["_id"]}.md"
+                file_path = path.join(output_folder, file_name)
 
-            tags = []
-            for tag in item["tags"]:
-                tags.append(tag["name"])
+                content = md(article["content"], strip=["h1"])
 
-            with open(file_path, "w") as file_object:
-                file_object.write("+++\n")
-                file_object.write(f'title = "{article["title"]}"\n')
-                file_object.write(f"date = '{item["authorship"]["creation_date"]}'\n")
-                file_object.write(f"lastmod = '{item["authorship"]["edition_date"]}'\n")
-                file_object.write(f"tags = {tags}\n")
-                # if "summary" in article:
-                #     file_object.write(f'summary = "{article["summary"]}"\n')
-                # file_object.write(f"draft = true\n")
-                file_object.write("+++\n")
-                file_object.write("\n")
+                content = process_content(content)
 
-                file_object.write(content)
+                tags = []
+                for tag in item["tags"]:
+                    tags.append(tag["name"])
 
-            print(article["title"])
+                # with open(file_path, "w") as file_object:
+                #     file_object.write("+++\n")
+                #     file_object.write(f'title = "{article["title"]}"\n')
+                #     file_object.write(f"date = '{item["authorship"]["creation_date"]}'\n")
+                #     file_object.write(f"lastmod = '{item["authorship"]["edition_date"]}'\n")
+                #     file_object.write(f"tags = {tags}\n")
+                #     # if "summary" in article:
+                #     #     file_object.write(f'summary = "{article["summary"]}"\n')
+                #     # file_object.write(f"draft = true\n")
+                #     file_object.write("+++\n")
+                #     file_object.write("\n")
+
+                #     file_object.write(content)
+
+                print(article["title"])
+
+        print("Records processing finished")
+    print("Session closed")
+print("Driver closed")
